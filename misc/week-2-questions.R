cbff271154a062cb0c4fb0000ecd32f5fa75da3b
David Keyes <david@rfortherestofus.com>
2024-03-21 12:08:36 -0700

    Update after week 2 live session

--- misc/week-2-questions.R
+++ misc/week-2-questions.R
@@ -9,6 +9,24 @@ getwd()
 
 penguins <- read_csv("data-raw/penguins.csv")
 
+penguins |> 
+  summarize(mean_weight = mean(body_mass_g, na.rm = TRUE))
+
+penguins |> 
+  count(island)
+
+penguins |> 
+  group_by(island, sex) |> 
+  summarize(number_of_penguins = n())
+
+torgersen_big_penguins <- penguins |> 
+  select(island, body_mass_g) |> 
+  filter(island == "Torgersen") |> 
+  filter(body_mass_g > 4000)
+
+torgersen_big_penguins |> 
+  write_csv("torgersen_big_penguins.csv")
+
 # Tidyverse pipe vs native pipe -------------------------------------------
 
 penguins |> 
@@ -25,6 +43,14 @@ big_penguins <-
 
 # Parentheses matter ------------------------------------------------------
 
+penguins |>
+  select(-species)
+
+penguins |>
+  select(-(bill_length_mm:body_mass_g))
+  
+
+
 read_csv("data-raw/penguins_data.csv", 
-         na = "-999, -999.0") |> 
-  view()
+         na = c("-999", "-999.0"))
+
---
+++ slides/render-slides.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(fs)
+library(pagedown)
+
+slides_files <-
+  dir_ls(regexp = ".html")
+
+html_to_pdf <- function(html_file) {
+  pdf_file_name <- str_remove(html_file, ".html")
+  
+  chrome_print(
+    input = html_file,
+    output = str_glue("pdf/{pdf_file_name}.pdf"),
+    timeout = 180
+  )
+}
+
+walk(slides_files, html_to_pdf)
