7043372511f64e9a867a913672759c465d17c9d5
David Keyes <david@rfortherestofus.com>
2024-05-22 17:00:52 -0700

    Week 11 slides

--- misc/mapping.R
+++ misc/mapping.R
@@ -1,9 +1,49 @@
 library(tidyverse)
+library(sf)
+
+
+# Portland ----------------------------------------------------------------
+
+portland_boundaries <-
+  read_sf("data-raw/City_Boundaries.geojson")
+
+traffic_signals <-
+  read_sf("data-raw/Traffic_Signals.geojson")
+
+snow_and_ice_routes <-
+  read_sf("data-raw/Snow_and_Ice_Routes.geojson")
+
+
+# Tigris ------------------------------------------------------------------
+
+library(tigris)
+
+us_states <- states()
+  
+# Median Income -----------------------------------------------------------
+
+library(tidycensus)
+
+median_income <-
+  get_acs(
+    state = "Oregon",
+    geography = "county",
+    variables = "B19013_001",
+    geometry = TRUE,
+    year = 2021
+  )
+
+median_income |>
+  ggplot(aes(fill = estimate)) +
+  geom_sf() +
+  scale_fill_viridis_c(option = "magma") +
+  theme_void()
+
+
+
+# International Data ------------------------------------------------------
+
 library(rnaturalearth)
-library(jardskjalftar)
-
-
-# Iceland Earthquakes -----------------------------------------------------
 
 iceland <-
   ne_countries(
@@ -11,52 +51,3 @@ iceland <-
     scale = "large",
     returnclass = "sf"
   )
-
-ggplot(data = iceland) +
-  geom_sf() +
-  theme_void()
-
-current_date <- today()
-current_year <- year(current_date)
-current_week <- week(current_date)
-
-earthquakes_this_week <- download_jsk_data(
-  year = current_year,
-  week = current_week
-)
-
-ggplot() +
-  geom_sf(data = iceland,
-          fill = "lightblue") +
-  geom_sf(data = earthquakes_this_week,
-          aes(color = depth)) +
-  theme_void()
-
-
-# Median Income -----------------------------------------------------------
-
-library(tidycensus)
-library(tidyverse)
-library(sf)
-
-median_income <-
-  get_acs(
-    state = "MI",
-    geography = "county",
-    variables = "B19013_001",
-    geometry = TRUE,
-    year = 2021
-  )
-
-median_income_points <-
-  median_income |>
-  st_centroid()
-
-median_income |>
-  ggplot(aes(fill = estimate)) +
-  # geom_sf() +
-  geom_sf(data = median_income_points,
-          color = "red",
-          aes(size = estimate)) +
-  scale_fill_viridis_c(option = "magma") +
-  theme_void()
--- misc/theme-dk.R
+++ misc/theme-dk.R
@@ -1,38 +1,28 @@
-theme_dk <- function(base_font = "Inter",
-                     hide_gridlines = TRUE,
-                     hide_legend = TRUE) {
-
-  update_geom_defaults(geom = "text",
-                       aes(family = base_font))
-
+theme_dk <- function(base_font = "Inter") {
   custom_theme <-
     theme_minimal(base_family = base_font) +
-    theme(axis.title = element_blank(),
-          axis.text = element_text(color = "grey60",
-                                   size = 10),
-          plot.title = element_markdown(),
-          plot.title.position = "plot",
-          legend.position = "none")
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_text(
+        color = "grey60",
+        size = 10
+      )
+    )
 
-
-  if (hide_gridlines == TRUE) {
-
-    custom_theme <-
-      custom_theme +
-      theme(panel.grid = element_blank())
-
-  }
-
-  if (hide_legend == FALSE) {
-
-    custom_theme <-
-      custom_theme +
-      theme(legend.position = "right")
-
-  }
-
-
-
-  return(custom_theme)
-
+  custom_theme
 }
+
+library(tidyverse)
+library(palmerpenguins)
+
+penguins |>
+  count(island) |>
+  ggplot(
+    aes(
+      x = island,
+      y = n,
+      fill = island
+    )
+  ) +
+  geom_col() +
+  theme_dk()
